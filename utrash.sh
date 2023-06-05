988a84b7b6a2a8b950ba412ce11d5ffc00e94767
Chandra Pham <chandrapham1@gmail.com>
2023-06-05 16:43:23 -0700

    added comments

--- utrash.sh
+++ utrash.sh
@@ -2,14 +2,14 @@
 
 #this only works if the target files/directories are in the pwd
 
-if ! [[ -d $HOME/.utrash ]] # if loop for... if .utrash directory does not exits
+if ! [[ -d $HOME/.utrash ]] # if .utrash directory does not exist
 then
     mkdir $HOME/.utrash # creates the directory!
 fi  
 
-if [[ "$1" = "rm" ]] && [[ $# -ge 2 ]] # if loop for... first argument is rm
+if [[ "$1" = "rm" ]] && [[ $# -ge 2 ]] # first argument is rm
 then
-    if [[ "$2" = "-r" ]] && [[ $# -eq 3 ]] # if loop for... argument is "rm -r dir"
+    if [[ "$2" = "-r" ]] && [[ $# -eq 3 ]] # argument is "rm -r dir"
     then
 	if ! [[ -d $PWD/$3 ]] # checking that directory exists
 	then
@@ -18,23 +18,23 @@ then
 	    cp -r "$3" $HOME/.utrash
 	    rm -fr "$3"
 	fi
-    elif [[ $# -eq 2 ]] # if loop for... argument is "rm file_name"
+    elif [[ $# -eq 2 ]] # argument is "rm file_name"
     then
 	if ! [[ -f $PWD/$2 ]]
 	then
 	    printf "Error: file not found; check that file is in the PWD\n" > /dev/stderr
-	else
+	else # copy file into trash, remove from pwd
 	    cp "$2" $HOME/.utrash
 	    rm "$2"
 	fi
     fi
-elif [[ $# = 1 ]] && [[ "$1" = "ls" ]] # if loop for... argument is "ls"
+elif [[ $# = 1 ]] && [[ "$1" = "ls" ]] # argument is "ls"
 then
     ls $HOME/.utrash
-elif [[ $# = 2 ]] && [[ "$1" = "dive" ]] # if loop for... argument is "dive [name]"
+elif [[ $# = 2 ]] && [[ "$1" = "dive" ]] # argument is "dive [name]"
 then
-    target="$(find "$HOME/.utrash" -name "$2")"
-    if [[ -d $target ]] # if loop for... trying to pull a directory
+    target="$(find "$HOME/.utrash" -name "$2")" # gets path to file in .utrash
+    if [[ -d $target ]] # trying to pull a directory
     then
 	cp -r "$target" $PWD
 	# if i wanted to delete it from the trash, i would add: rm -r $target
@@ -45,12 +45,12 @@ then
     else # if the target is not in the utrash directory
 	printf "Error: file/directory not found in utrash; try the './utrash ls' command to check trash contents\n" > /dev/stderr
     fi
-elif [[ $# = 1 ]] && [ "$1" = "dump" ] # if loop for... argument is "dump"
-then
+elif [[ $# = 1 ]] && [ "$1" = "dump" ] # argument is "dump"
+then # remove everything in the .utrash directory
     rm -fr $HOME/.utrash/*
-elif [[ $# = 1 ]] && [ "$1" = "--help" ]
+elif [[ $# = 1 ]] && [ "$1" = "--help" ] # the --help info
 then
     printf "Usage:\n  utrash rm [file name]  -  removes file, stores copy in trash\n  utrash rm -r [directory name]  -  removes subdirectory, stores copy in trash\n  utrash ls  -  lists files currently in trash\n  utrash dive [name]  -  copies file/directory from trash into PWD\n  utrash dump  -  empties trash (permanent!)\n* Files/directories must be in PWD to be moved to trash *\n"
-    else
+    else # if the arguments do not match any expected arguments
     printf "Error: unexpected arguements ; use --help command for guidance\n" > /dev/stderr
 fi
